fe591d4bb6f04b9337f74e91b03be664d43ad5d2
mayrant <mayrant2004@yahoo.com>
2021-10-12 22:57:48 -0500

    added business data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
+import { businesses } from './data/businesses';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState(businesses);
+
+  useEffect(() => {
+    console.log("business list:", list);
+  }, [list]);
+
   return (
     <div className="App">
-      
+      <ul>
+        {businesses.map((business) => {
+          return <li key={business.id}>{business.name}</li>
+        })}
+      </ul>
     </div>
   );
 }

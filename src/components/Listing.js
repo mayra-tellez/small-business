7d4fc024dddd881c3d5f253453ab8b8f6a059ded
mayrant <mayrant2004@yahoo.com>
2021-10-14 11:45:52 -0500

    converted data to table in listing component, still needs formatting

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@material-ui/core'
 
 import { businesses } from '../data/businesses';
 
@@ -11,11 +20,33 @@ function Listing() {
   }, [list]);
 
   return (
-    <ul>
-      {businesses.map((business) => {
-        return <li key={business.id}>{business.name}</li>
-      })}
-    </ul>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Hours</TableCell>
+            <TableCell>Address</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {list.map((business) => (
+            <TableRow
+              key={business.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {business.name}
+              </TableCell>
+              <TableCell>{business.description}</TableCell>
+              <TableCell>{business.operatingHours}</TableCell>
+              <TableCell>{business.address}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 

1f2282cb793d8d620e2166120147fa2d21a28263
mayrant <mayrant2004@yahoo.com>
2021-10-20 18:20:26 -0500

    redux setup

---
+++ src/redux/reducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+const user = (state = null) => state;
+const businesses = (state = []) => state;
+
+export default combineReducers({ user, businesses });
---
+++ src/redux/state.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  user: {
+    username: "Example User",
+    email: "exampleuser@email.com"
+  },
+  businesses: [
+    {
+      "id": 0,
+      "name": "Cosmic Coffee & Beer Garden",
+      "description": "Specialty coffee, local beers, craft cocktails & live music plus food trucks in a garden setting.",
+      "address": "121 Pickle Rd, Austin, TX 78704",
+      "operatingHours": ""
+    },
+    {
+      "id": 1,
+      "name": "Wildflowers Boutique",
+      "description": "Cozy local boutique featuring a stylish range of women's apparel, jewelry, home goods & gifts.",
+      "address": "3800 N Lamar Blvd #145, Austin, TX 78756",
+      "operatingHours": ""
+    },
+    {
+      "id": 2,
+      "name": "Noonday Collection",
+      "description": "",
+      "address": "1825 E 38th 1/2 St, Austin, TX 78722",
+      "operatingHours": ""
+    },
+    {
+      "id": 3,
+      "name": "Black Rock Coffee Bar",
+      "description": "Coffee chain serving classic & specialty beverages via walk-up counter (some drive-thru locations).",
+      "address": "7415 Southwest Pkwy BLDG 6, Suite 700, Austin, TX 78735",
+      "operatingHours": ""
+    },
+    {
+      "id": 4,
+      "name": "Intelligent Engineering Services",
+      "description": "",
+      "address": "",
+      "operatingHours": ""
+    }
+  ]
+}

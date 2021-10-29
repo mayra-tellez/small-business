aa5b72a7f246eb608300e13a4ec6e68c12e8eda0
mayrant <mayrant2004@yahoo.com>
2021-10-29 13:07:06 -0500

    updated business data

--- src/data/businesses.js
+++ src/data/businesses.js
@@ -4,34 +4,34 @@ export const businesses = [
     name: "Cosmic Coffee & Beer Garden",
     description: "Specialty coffee, local beers, craft cocktails & live music plus food trucks in a garden setting.",
     address: "121 Pickle Rd, Austin, TX 78704",
-    operatingHours: ""
+    operatingHours: "8AM - 11PM"
   },
   {
     id: 1,
     name: "Wildflowers Boutique",
     description: "Cozy local boutique featuring a stylish range of women's apparel, jewelry, home goods & gifts.",
     address: "3800 N Lamar Blvd #145, Austin, TX 78756",
-    operatingHours: ""
+    operatingHours: "10AM - 5PM"
   },
   {
     id: 2,
     name: "Noonday Collection",
-    description: "",
+    description: "Handcrafted jewelry and accessories. Styled and sold by ambassadors. Empowering artisan businesses globally.",
     address: "1825 E 38th 1/2 St, Austin, TX 78722",
-    operatingHours: ""
+    operatingHours: "9AM - 5PM"
   },
   {
     id: 3,
     name: "Black Rock Coffee Bar",
     description: "Coffee chain serving classic & specialty beverages via walk-up counter (some drive-thru locations).",
     address: "7415 Southwest Pkwy BLDG 6, Suite 700, Austin, TX 78735",
-    operatingHours: ""
+    operatingHours: "5AM - 9PM"
   },
   {
     id: 4,
     name: "Intelligent Engineering Services",
-    description: "",
-    address: "",
-    operatingHours: ""
+    description: "Dedicated to delivering quality structural and civil engineering services that bring architectural visions to life.",
+    address: "2021 Guadalupe Street, Suite 260, Austin, TX 78705",
+    operatingHours: "8AM - 5PM"
   }
 ];

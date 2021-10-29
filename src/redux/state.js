84b302fae4f3b3d008ece96d39dcc17bcb4ddda0
mayrant <mayrant2004@yahoo.com>
2021-10-29 13:10:42 -0500

    updated business data in state, deleted data folder

--- src/redux/state.js
+++ src/redux/state.js
@@ -3,39 +3,39 @@ export default {
   user: { },
   businesses: [
     {
-      "id": 0,
-      "name": "Cosmic Coffee & Beer Garden",
-      "description": "Specialty coffee, local beers, craft cocktails & live music plus food trucks in a garden setting.",
-      "address": "121 Pickle Rd, Austin, TX 78704",
-      "operatingHours": ""
+      id: 0,
+      name: "Cosmic Coffee & Beer Garden",
+      description: "Specialty coffee, local beers, craft cocktails & live music plus food trucks in a garden setting.",
+      address: "121 Pickle Rd, Austin, TX 78704",
+      operatingHours: "8AM - 11PM"
     },
     {
-      "id": 1,
-      "name": "Wildflowers Boutique",
-      "description": "Cozy local boutique featuring a stylish range of women's apparel, jewelry, home goods & gifts.",
-      "address": "3800 N Lamar Blvd #145, Austin, TX 78756",
-      "operatingHours": ""
+      id: 1,
+      name: "Wildflowers Boutique",
+      description: "Cozy local boutique featuring a stylish range of women's apparel, jewelry, home goods & gifts.",
+      address: "3800 N Lamar Blvd #145, Austin, TX 78756",
+      operatingHours: "10AM - 5PM"
     },
     {
-      "id": 2,
-      "name": "Noonday Collection",
-      "description": "",
-      "address": "1825 E 38th 1/2 St, Austin, TX 78722",
-      "operatingHours": ""
+      id: 2,
+      name: "Noonday Collection",
+      description: "Handcrafted jewelry and accessories. Styled and sold by ambassadors. Empowering artisan businesses globally.",
+      address: "1825 E 38th 1/2 St, Austin, TX 78722",
+      operatingHours: "9AM - 5PM"
     },
     {
-      "id": 3,
-      "name": "Black Rock Coffee Bar",
-      "description": "Coffee chain serving classic & specialty beverages via walk-up counter (some drive-thru locations).",
-      "address": "7415 Southwest Pkwy BLDG 6, Suite 700, Austin, TX 78735",
-      "operatingHours": ""
+      id: 3,
+      name: "Black Rock Coffee Bar",
+      description: "Coffee chain serving classic & specialty beverages via walk-up counter (some drive-thru locations).",
+      address: "7415 Southwest Pkwy BLDG 6, Suite 700, Austin, TX 78735",
+      operatingHours: "5AM - 9PM"
     },
     {
-      "id": 4,
-      "name": "Intelligent Engineering Services",
-      "description": "",
-      "address": "",
-      "operatingHours": ""
+      id: 4,
+      name: "Intelligent Engineering Services",
+      description: "Dedicated to delivering quality structural and civil engineering services that bring architectural visions to life.",
+      address: "2021 Guadalupe Street, Suite 260, Austin, TX 78705",
+      operatingHours: "8AM - 5PM"
     }
   ]
 }
